af52a1b92aace01b5fc6e03d462a6d79a43a8751
Shehryar Assad <xshehryar@gmail.com>
2019-01-23 09:29:09 -0500

    add register template

--- django_first_project/settings.py
+++ django_first_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',  # searches for templates now
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
