903a1b6334a9154eff38173c7913f6541cafbabc
Shehryar Assad <xshehryar@gmail.com>
2019-01-23 15:52:59 -0500

    add profile page

--- django_first_project/urls.py
+++ django_first_project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url('register/', user_views.register, name='register'),
     url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url('profile/', user_views.profile, name='profile'),
     url('', include('blog.urls')),
 ]
