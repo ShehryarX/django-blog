c86159d33c945e674e5cf931d9375add3bc06c6f
Shehryar Assad <xshehryar@gmail.com>
2019-01-22 21:44:21 -0500

    add html teplates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Shehryar',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'January 21, 2019'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 22, 2019'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
