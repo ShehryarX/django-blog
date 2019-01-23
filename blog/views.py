055c988a085c8448b1c0ad9e80a22a1fe3ef8224
Shehryar Assad <xshehryar@gmail.com>
2019-01-22 22:47:54 -0500

    Add Post to admin panel

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
+from .models import Post  # . means current directory
 
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Shehryar',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'January 21, 2019'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 22, 2019'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
