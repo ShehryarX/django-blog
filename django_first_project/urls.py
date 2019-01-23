d941208a0dea91f31c30cd342ff5b0fb31674b57
Shehryar Assad <xshehryar@gmail.com>
2019-01-23 16:15:35 -0500

    Add profile picture

--- django_first_project/urls.py
+++ django_first_project/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,5 +26,9 @@ urlpatterns = [
     url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url('profile/', user_views.profile, name='profile'),
-    url('', include('blog.urls')),
+    url('', include('blog.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
